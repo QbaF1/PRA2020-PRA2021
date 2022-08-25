351c5bfbad422c23531f63946eaa639049271cd6
QbaF1 <kuba_f1bmw@o2.pl>
2022-08-25 12:55:31 +0200

    v1.0

--- src/main/java/introduction/HelloWorld.java
+++ src/main/java/introduction/HelloWorld.java
@@ -1,8 +1,13 @@
 package introduction;
 
+import java.util.logging.Logger;
+
 public class HelloWorld {
 
     public static void main(String [ ] args) {
         System.out.println("Hello World");
+
+        Logger log = Logger.getLogger("name");
+        log.info("message");
     }
 }
